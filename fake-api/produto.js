536ea10a18c7a1a664da39c0fa3ccc1310452386
janildo1 <janildo@outlook.com>
2022-12-01 19:58:18 -0300

    api fake com um arquivo js

---
+++ fake-api/produto.js
@@ -0,0 +1,7 @@
+[
+    { "nome": "Chocolate branco com Oreo", "img": "https://janildo1.github.io/prj-frontend/fake-api/img/chocolate-oreo.webp", "valor": 41.6 },
+    { "nome": "Brownie com brigadeiro de limão", "img": "https://janildo1.github.io/prj-frontend/fake-api/img/brigadeiro-limao.webp", "valor": 42.5 },
+    { "nome": "Brigadeiro ao leite com maracujá", "img": "https://janildo1.github.io/prj-frontend/fake-api/img/brigadeiro-maracuja.webp", "valor": 30.8 },
+    { "nome": "Brigadeiro ao leite com beijinho", "img": "https://janildo1.github.io/prj-frontend/fake-api/img/brigadeiro-beijinho.webp", "valor": 40.5 },
+    { "nome": "Leite Ninho com Nutela", "img": "https://janildo1.github.io/prj-frontend/fake-api/img/nutela.webp", "valor": 45.0 }
+]
