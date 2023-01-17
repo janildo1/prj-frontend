8b248a4689ae4771bbbdca04b6cc55da6ccb5af9
janildo1 <janildo@outlook.com>
2023-01-17 20:07:16 -0300

    Ajuste no arquivo produto2

--- fake-api/produto2.js
+++ fake-api/produto2.js
@@ -1,8 +1,8 @@
 [
-    { "nome": "Bicicleta Aro 29 GTI", "img": "https://janildo1.github.io/prj-frontend/fake-api/img/bicicleta-GTI.webp", "valor": 1300.00 },
-    { "nome": "Bike BMX", "img": "https://janildo1.github.io/prj-frontend/fake-api/img/bmx-bike.webp", "valor": 800.00 },
-    { "nome": "Câmera de ar", "img": "https://janildo1.github.io/prj-frontend/fake-api/img/camera-de-ar.webp", "valor": 30.00 },
-    { "nome": "Pneu Aro 29", "img": "https://janildo1.github.io/prj-frontend/fake-api/img/pneu-aro29.webp", "valor": 60.00 },
-    { "nome": "Quadro Aro 29 Venzo", "img": "https://janildo1.github.io/prj-frontend/fake-api/img/quadro-venzo.webp", "valor": 500.00 },
-    { "nome": "óculos de Sol", "img": "https://janildo1.github.io/prj-frontend/fake-api/img/oculos.webp", "valor": 150.00 }
+    { "nome": "Bicicleta Aro 29 GTI", "img": "https://janildo1.github.io/prj-frontend/fake-api/img/bicicleta-GTI.jpg", "valor": 1300.00 },
+    { "nome": "Bike BMX", "img": "https://janildo1.github.io/prj-frontend/fake-api/img/bmx-bike.jpg", "valor": 800.00 },
+    { "nome": "Câmera de ar", "img": "https://janildo1.github.io/prj-frontend/fake-api/img/camera-de-ar.jpg", "valor": 30.00 },
+    { "nome": "Pneu Aro 29", "img": "https://janildo1.github.io/prj-frontend/fake-api/img/pneu-aro29..jfif", "valor": 60.00 },
+    { "nome": "Quadro Aro 29 Venzo", "img": "https://janildo1.github.io/prj-frontend/fake-api/img/quadro-venzo.jpg", "valor": 500.00 },
+    { "nome": "óculos de Sol", "img": "https://janildo1.github.io/prj-frontend/fake-api/img/oculos.jpg", "valor": 150.00 }
 ]
