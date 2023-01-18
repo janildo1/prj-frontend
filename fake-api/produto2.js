1c40dd6329f6823a8ca634846b06a4b320f25c65
janildo1 <janildo@outlook.com>
2023-01-18 17:56:31 -0300

    Atualizando script

--- fake-api/produto2.js
+++ fake-api/produto2.js
@@ -2,7 +2,7 @@
     { "nome": "Bicicleta Aro 29 GTI", "img": "https://janildo1.github.io/prj-frontend/fake-api/img/bicicleta-GTI.jpg", "valor": 1300.00 },
     { "nome": "Bike BMX", "img": "https://janildo1.github.io/prj-frontend/fake-api/img/bmx-bike.jpg", "valor": 800.00 },
     { "nome": "Câmera de ar", "img": "https://janildo1.github.io/prj-frontend/fake-api/img/camera-de-ar.jpg", "valor": 30.00 },
-    { "nome": "Pneu Aro 29", "img": "https://janildo1.github.io/prj-frontend/fake-api/img/pneu-aro29..jfif", "valor": 60.00 },
+    { "nome": "Pneu Aro 29", "img": "https://janildo1.github.io/prj-frontend/fake-api/img/pneu-aro29.jpg", "valor": 60.00 },
     { "nome": "Quadro Aro 29 Venzo", "img": "https://janildo1.github.io/prj-frontend/fake-api/img/quadro-venzo.jpg", "valor": 500.00 },
     { "nome": "óculos de Sol", "img": "https://janildo1.github.io/prj-frontend/fake-api/img/oculos.jpg", "valor": 150.00 }
 ]
